628a79bae6e094b2805f8a01d3182ce4bf6beb0d
Neves007 <2214519556@qq.com>
2019-03-27 21:24:22 +0800

    login 的response可以拿到，但是重定向有问题const path = util.cookies.get('redirect')

---
+++ lano/lano_end/views/user_views.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import HttpResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
+from lano_end.models import User
+import json
+
+
+def uuid_tostr(user):
+    return user.uuid.__str__()
+
+
+def user_format(user):
+    return {
+        'uuid': uuid_tostr(user),
+        'username': user.username,
+        'password': user.password,
+        'name': user.name,
+    }
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def login(request):
+    obj = json.loads(request.body)
+    username = obj['username']
+    password = obj['password']
+    user = User.objects.get(username=username)
+    user = user_format(user)
+    response = {}
+    try:
+        if user:
+            if password == user['password']:
+                user['tocken']='d787syv8dys8cas80d9s0a0d8f79ads56f7s4d56f879a8as89fd980s7dg'
+                response['code'] = 0
+                response['msg'] = '登录成功'
+                response['data'] = {'user':user, 'tocken': user['tocken']}
+                response['error_num'] = 0
+            else:
+                response['code'] = '401'
+                response['msg'] = '密码错误'
+                response['data'] = {}
+                response['error_num'] = 1
+        else:
+            response['code'] = 401
+            response['msg'] = '登录失败'
+            response['data'] = {}
+            response['error_num'] = 1
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
